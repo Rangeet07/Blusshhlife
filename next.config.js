a417aded06dd7819bb4a17be3b56b46dcf7d46b6
Rangeet07 <rangeetnandy07@gmail.com>
2024-01-24 23:57:43 +0530

    remotepatternsnextconfigandapi

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental:{
+        esmExternals: "loose",
+        serverComponentsExternalPackages: ["mongoose"],
+    },
     images:{
         // domains:['scontent.cdninstagram.com','images.unsplash.com']
         remotePatterns: [
             {
               protocol: 'https',
               hostname: '**.cdninstagram.com',
+              port: '',
+              pathname: '**',
+            },
+            {
+              protocol: 'https',
+              hostname: 'instagram.flba2-1.fna.fbcdn.net',
               port: '',
               pathname: '**',
             },
