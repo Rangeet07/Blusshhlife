90c37c9bf897f99fd1e38904cbb99ea007fe7445
Rangeet07 <rangeetnandy07@gmail.com>
2024-01-24 20:53:03 +0530

    remotepatternsnextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['scontent.cdninstagram.com','images.unsplash.com']
+        // domains:['scontent.cdninstagram.com','images.unsplash.com']
+        remotePatterns: [
+            {
+              protocol: 'https',
+              hostname: 'scontent.cdninstagram.com',
+              port: '',
+              pathname: '**',
+            },
+          
+          ],
     }
 }
 module.exports = nextConfig
